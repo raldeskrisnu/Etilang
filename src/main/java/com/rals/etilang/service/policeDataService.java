4d9a594dd798b3afa5719726be4a0a309019a7e7
raldes krisnu <raldeskrisnu@gmail.com>
2018-01-31 17:35:05 +0700

    delete unused code

--- src/main/java/com/rals/etilang/service/policeDataService.java
+++
@@ -1,9 +0,0 @@
-package com.rals.etilang.service;
-
-import com.rals.etilang.model.modelPoliceData;
-
-public interface policeDataService extends policeDataCrudService<modelPoliceData>{
-	
-
-}
-
--- src/main/java/com/rals/etilang/service/policeLocationService.java
+++
@@ -1,7 +0,0 @@
-package com.rals.etilang.service;
-
-import com.rals.etilang.model.modelPoliceLocation;
-
-public interface policeLocationService extends policeLocationCrudService<modelPoliceLocation> {
-
-}
