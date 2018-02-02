9ed1b9a3dbb4c840dbe97d650ed0e82fa854a14e
raldes krisnu <raldeskrisnu@gmail.com>
2018-02-02 10:56:47 +0700

    add kejaksaan controller

---
+++ src/main/java/com/rals/etilang/controller/kejaksaanController.java
@@ -0,0 +1,31 @@
+package com.rals.etilang.controller;
+
+import com.rals.etilang.model.modelKejaksaanNegeri;
+import com.rals.etilang.model.modelPoliceData;
+import com.rals.etilang.service.kejaksaanNegeriData.kejaksaanNegeriService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class kejaksaanController {
+
+    @Autowired
+    private kejaksaanNegeriService kejaksaanNegeriService;
+
+    @RequestMapping(value = "/getDataKejaksaanNegeri", method = RequestMethod.GET)
+    public ResponseEntity<List<modelKejaksaanNegeri>> displayKejaksaanNegeri() {
+        List<modelKejaksaanNegeri> employees = kejaksaanNegeriService.getDataKejaksaanNegeri();
+        if (employees.isEmpty()) {
+
+            return new ResponseEntity<List<modelKejaksaanNegeri>>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<List<modelKejaksaanNegeri>>(employees, HttpStatus.OK);
+    }
+}
